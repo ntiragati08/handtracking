f2db3247cf9dacb47b02e2e75d456d64789f1163
ntiragati08 <98192697+ntiragati08@users.noreply.github.com>
2022-08-07 13:41:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,25 +6,21 @@ import streamlit as st
 from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
 from aiortc.contrib.media import MediaRecorder
 
-def main():
-    def recorder_factory():
-        return MediaRecorder("record.wav")
+def recorder_factory():
+    return MediaRecorder("record.wav")
 
-    webrtc_streamer(
-        key="sendonly-audio",
-        mode=WebRtcMode.SENDONLY,
-        in_recorder_factory=recorder_factory,
-        client_settings=ClientSettings(
-            rtc_configuration={
-                "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-            },
-            media_stream_constraints={
-                "audio": True,
-                "video": False,
-            },
-        ),
-    )
+webrtc_streamer(
+    key="sendonly-audio",
+    mode=WebRtcMode.SENDONLY,
+    in_recorder_factory=recorder_factory,
+    client_settings=ClientSettings(
+        rtc_configuration={
+            "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+        },
+        media_stream_constraints={
+            "audio": True,
+            "video": False,
+        },
+    ),
+)
 
-
-if __name__ == "__main__":
-    main()
